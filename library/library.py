8d3ab264180ae07f9244a817451f5b64b9240d77
Gillen Brown <gillenb@umich.edu>
2020-04-22 20:44:52 -0400

    add paper function

--- library/library.py
+++ library/library.py
@@ -1,6 +1,11 @@
+import work
+
 class Library(object):
     def __init__(self):
         self.papers = []
+
+    def add_paper(self, paper_url):
+        self.papers.append(work.Paper(paper_url))
 
 
 # follow this to generate API key https://ads.readthedocs.io/en/latest/
