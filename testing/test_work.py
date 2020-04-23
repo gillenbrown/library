0e3b3476f855e4076024b750987ffb318a06a86e
Gillen Brown <gillenb@umich.edu>
2020-04-22 22:56:01 -0400

    redid cache tests
    I don't care about details of what's in the cache, only how it behaves, so some tests were removed. The number of queries is tested to be stable, though

--- testing/test_work.py
+++ testing/test_work.py
@@ -7,6 +7,22 @@ import test_utils as u
 def fixture_ads_paper():
     return Paper(u.my_ads_url)
 
+def test_cache_no_extra_queries(paper):
+    # here I'll query the same pre-existing paper with multiple URLs to ensure
+    # that the caching is working as expected I first have to make sure
+    # everything is in the cache, so I need to do all needed searches, then I
+    # can duplicate them
+    _ = Paper(u.my_arxiv_url)
+    queries_start = paper.num_queries
+    _ = Paper(u.my_ads_url)
+    _ = Paper(u.my_arxiv_url)
+    p = Paper(u.my_arxiv_pdf_url)
+    queries_new = paper.num_queries
+    queries_other = p.num_queries  # check that it's shared between objects
+
+    assert queries_new == queries_start
+    assert queries_new == queries_other
+
 def test_get_bibtex_from_ads(paper):
     assert paper.bibtex_entry == u.my_bibtex
 
@@ -20,25 +36,6 @@ def test_create_paper_from_arxiv_pdf(paper):
     # same as above test, just with arxiv link to the pdf
     assert arxiv_paper.bibtex_entry == paper.bibtex_entry
 
-def test_bibtex_cache(paper):
-    # this cache will already be created.
-    assert paper._bibtexs_from_bibcode[u.my_bibcode] == u.my_bibtex
 
-def test_arxiv_to_bibcode_cache(paper):
-    # to make sure the cache exists I'll add the paper via arxiv link
-    _ = Paper(u.my_arxiv_pdf_url)
-    # then test the original cache, to ensure it's shared
-    assert paper._bibcode_from_arxiv_id[u.my_arxiv_id] == u.my_bibcode
-
-def test_no_extra_queries(paper):
-    # here I'll query the same pre-existing paper with multiple URLs to ensure
-    # that the caching is working as expected
-    queries_start = paper.num_queries
-    _ = Paper(u.my_ads_url)
-    _ = Paper(u.my_arxiv_url)
-    p = Paper(u.my_arxiv_pdf_url)
-    queries_new = p.num_queries
-
-    assert queries_new == queries_start
 
 #TODO: test that adding papers from one of multiple ADS links gives the same paper
