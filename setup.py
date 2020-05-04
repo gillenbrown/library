b6e2f593ef2d2a48838d17528383060f940fc46e
Gillen Brown <gillenb@umich.edu>
2020-05-04 18:59:15 -0400

    cleaned up setup.py

--- setup.py
+++ setup.py
@@ -6,8 +6,5 @@ setup(
 	author="Gillen Brown",
 	author_email="gillenbrown@gmail.com",
 	packages=find_packages(exclude=["testing"]),
-	install_requires=["ads"],
-    tests_require=["pytest",
-                   "python-coveralls"],
-    test_suite="pytest"
+	install_requires=["ads"]
 	)
