68decb3e5878e75ec194e2d5e98404330b38f0dc
Gillen Brown <gillenb@umich.edu>
2020-05-31 15:08:28 -0400

    went back to qt5.15, tried some extra packages found elsewhere

--- setup.py
+++ setup.py
@@ -6,6 +6,6 @@ setup(
     author="Gillen Brown",
     author_email="gillenbrown@gmail.com",
     packages=find_packages(exclude=["testing"]),
-    install_requires=["ads", "pyside2==5.14.2.2"],
+    install_requires=["ads", "pyside2"],
     python_requires=">=3.7",
 )
