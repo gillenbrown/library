0722c0e9b8330e1e3aabe7886d0b1e5b1bc46079
Gillen Brown <gillenbrown@gmail.com>
2022-10-07 19:35:26 -0600

    tweak to paperdict
    raises an error if the item is not found, rather than returning None

--- testing/test_utils.py
+++ testing/test_utils.py
@@ -3,9 +3,11 @@
 class PaperDict(dict):
     """dot.notation access to dictionary attributes"""
 
-    __getattr__ = dict.get
     __setattr__ = dict.__setitem__
     __delattr__ = dict.__delitem__
+
+    def __getattr__(self, item):
+        return self[item]
 
 
 # Use my paper as the main testing paper.
