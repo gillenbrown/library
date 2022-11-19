547d43561719dcc6720aada8bf24170b2993c9a1
Gillen Brown <gillenbrown@gmail.com>
2022-11-19 16:13:43 -0700

    fixed placeholder text transparency
    setting the text color of the QLineEdit with a stylesheet makes the placeholder text the same color, which is not useful. This complicated method fixes that

--- testing/test_interface.py
+++ testing/test_interface.py
@@ -310,6 +310,18 @@ def cEditCiteKey(mainWidget, citeKey, qtbot):
     cPressEnter(mainWidget.rightPanel.editCiteKeyEntry, qtbot)
 
 
+def cGetTextAlpha(widget):
+    """
+    Get the alpha value of the text in a widget (0=clear, 255=opaque)
+
+    :param widget: The widget to check
+    :type widget: QWidget
+    :return: the alpha value
+    :rtype: int
+    """
+    return widget.palette().color(QPalette.Text).toRgb().toTuple()[-1]
+
+
 # ======================================================================================
 #
 # mock functions for use with monkeypatch
@@ -691,6 +703,150 @@ def test_add_button_and_search_bar_are_much_shorter_than_title(qtbot, db_empty):
     widget = cInitialize(qtbot, db_empty)
     assert widget.addButton.height() < 0.6 * widget.title.height()
     assert widget.searchBar.height() < 0.6 * widget.title.height()
+
+
+# ==========================
+# colors of placeholder text
+# ==========================
+def test_searchbar_placeholder_text_starts_transparent(qtbot, db):
+    widget = cInitialize(qtbot, db)
+    assert cGetTextAlpha(widget.searchBar) == 100
+
+
+def test_searchbar_text_is_not_transparent_once_modified(qtbot, db):
+    widget = cInitialize(qtbot, db)
+    cEnterText(widget.searchBar, "g", qtbot)
+    assert cGetTextAlpha(widget.searchBar) == 255
+
+
+def test_searchbar_text_is_transparent_once_cleared(qtbot, db):
+    widget = cInitialize(qtbot, db)
+    cEnterText(widget.searchBar, "g", qtbot)
+    cPressBackspace(widget.searchBar, qtbot)
+    assert cGetTextAlpha(widget.searchBar) == 100
+
+
+def test_add_tag_placeholder_text_starts_transparent(qtbot, db):
+    widget = cInitialize(qtbot, db)
+    cClick(widget.tagsList.addTagButton, qtbot)
+    assert cGetTextAlpha(widget.tagsList.addTagBar) == 100
+
+
+def test_add_tag_text_is_not_transparent_once_modified(qtbot, db):
+    widget = cInitialize(qtbot, db)
+    cClick(widget.tagsList.addTagButton, qtbot)
+    cEnterText(widget.tagsList.addTagBar, "g", qtbot)
+    assert cGetTextAlpha(widget.tagsList.addTagBar) == 255
+
+
+def test_add_tag_text_is_transparent_once_cleared(qtbot, db):
+    widget = cInitialize(qtbot, db)
+    cClick(widget.tagsList.addTagButton, qtbot)
+    cEnterText(widget.tagsList.addTagBar, "g", qtbot)
+    cPressBackspace(widget.tagsList.addTagBar, qtbot)
+    assert cGetTextAlpha(widget.tagsList.addTagBar) == 100
+
+
+def test_delete_tag_placeholder_text_starts_transparent(qtbot, db):
+    widget = cInitialize(qtbot, db)
+    cClick(widget.tagsList.firstDeleteTagButton, qtbot)
+    assert cGetTextAlpha(widget.tagsList.secondDeleteTagEntry) == 100
+
+
+def test_delete_tag_text_is_not_transparent_once_modified(qtbot, db):
+    widget = cInitialize(qtbot, db)
+    cClick(widget.tagsList.firstDeleteTagButton, qtbot)
+    cEnterText(widget.tagsList.secondDeleteTagEntry, "g", qtbot)
+    assert cGetTextAlpha(widget.tagsList.secondDeleteTagEntry) == 255
+
+
+def test_delete_tag_text_is_transparent_once_cleared(qtbot, db):
+    widget = cInitialize(qtbot, db)
+    cClick(widget.tagsList.firstDeleteTagButton, qtbot)
+    cEnterText(widget.tagsList.secondDeleteTagEntry, "g", qtbot)
+    cPressBackspace(widget.tagsList.secondDeleteTagEntry, qtbot)
+    assert cGetTextAlpha(widget.tagsList.secondDeleteTagEntry) == 100
+
+
+def test_cite_key_placeholder_text_is_transparent(qtbot, db_temp):
+    # click on a paper with no cite key set
+    widget = cInitialize(qtbot, db_temp)
+    cClick(widget.papersList.getPapers()[0], qtbot)
+    cClick(widget.rightPanel.editCiteKeyButton, qtbot)
+    assert cGetTextAlpha(widget.rightPanel.editCiteKeyEntry) == 100
+
+
+def test_cite_key_placeholder_text_is_not_transparent_when_modified(qtbot, db_temp):
+    # click on a paper with no cite key set
+    widget = cInitialize(qtbot, db_temp)
+    cClick(widget.papersList.getPapers()[0], qtbot)
+    cClick(widget.rightPanel.editCiteKeyButton, qtbot)
+    cEnterText(widget.rightPanel.editCiteKeyEntry, "g", qtbot)
+    assert cGetTextAlpha(widget.rightPanel.editCiteKeyEntry) == 255
+
+
+def test_cite_key_placeholder_text_is_transparent_when_cleared(qtbot, db_temp):
+    # click on a paper with no cite key set
+    widget = cInitialize(qtbot, db_temp)
+    cClick(widget.papersList.getPapers()[0], qtbot)
+    cClick(widget.rightPanel.editCiteKeyButton, qtbot)
+    cEnterText(widget.rightPanel.editCiteKeyEntry, "g", qtbot)
+    cPressBackspace(widget.rightPanel.editCiteKeyEntry, qtbot)
+    assert cGetTextAlpha(widget.rightPanel.editCiteKeyEntry) == 100
+
+
+def test_cite_key_not_transparent_if_set(qtbot, db_empty):
+    # click on a paper with a cite key set
+    db_empty.add_paper(u.mine.bibcode)
+    db_empty.set_paper_attribute(u.mine.bibcode, "citation_keyword", "test")
+    widget = cInitialize(qtbot, db_empty)
+    cClick(widget.papersList.getPapers()[0], qtbot)
+    cClick(widget.rightPanel.editCiteKeyButton, qtbot)
+    assert cGetTextAlpha(widget.rightPanel.editCiteKeyEntry) == 255
+
+
+def test_cite_key_transparent_correct_new_paper_clear_to_clear(qtbot, db_temp):
+    # test when transitioning from one paper without a key to another without a key
+    widget = cInitialize(qtbot, db_temp)
+    cClick(widget.papersList.getPapers()[0], qtbot)
+    cClick(widget.rightPanel.editCiteKeyButton, qtbot)
+    cClick(widget.papersList.getPapers()[1], qtbot)
+    cClick(widget.rightPanel.editCiteKeyButton, qtbot)
+    assert cGetTextAlpha(widget.rightPanel.editCiteKeyEntry) == 100
+
+
+def test_cite_key_transparent_correct_new_paper_clear_to_full(qtbot, db_temp):
+    # test when transitioning from one paper without a key to another with a key
+    db_temp.set_paper_attribute(u.tremonti.bibcode, "citation_keyword", "test")
+    widget = cInitialize(qtbot, db_temp)
+    cClick(widget.papersList.getPapers()[1], qtbot)  # mine
+    cClick(widget.rightPanel.editCiteKeyButton, qtbot)
+    cClick(widget.papersList.getPapers()[0], qtbot)  # tremonti
+    cClick(widget.rightPanel.editCiteKeyButton, qtbot)
+    assert cGetTextAlpha(widget.rightPanel.editCiteKeyEntry) == 255
+
+
+def test_cite_key_transparent_correct_new_paper_full_to_clear(qtbot, db_temp):
+    # test when transitioning from one paper with a key to another without a key
+    widget = cInitialize(qtbot, db_temp)
+    cClick(widget.papersList.getPapers()[0], qtbot)
+    cClick(widget.rightPanel.editCiteKeyButton, qtbot)
+    cEnterText(widget.rightPanel.editCiteKeyEntry, "sdf", qtbot)
+    cClick(widget.papersList.getPapers()[1], qtbot)
+    cClick(widget.rightPanel.editCiteKeyButton, qtbot)
+    assert cGetTextAlpha(widget.rightPanel.editCiteKeyEntry) == 100
+
+
+def test_cite_key_transparent_correct_new_paper_full_to_full(qtbot, db_temp):
+    # test when transitioning from one paper with a key to another with a key
+    db_temp.set_paper_attribute(u.tremonti.bibcode, "citation_keyword", "test")
+    widget = cInitialize(qtbot, db_temp)
+    cClick(widget.papersList.getPapers()[1], qtbot)  # mine
+    cClick(widget.rightPanel.editCiteKeyButton, qtbot)
+    cEnterText(widget.rightPanel.editCiteKeyEntry, "sdf", qtbot)
+    cClick(widget.papersList.getPapers()[0], qtbot)  # tremonti
+    cClick(widget.rightPanel.editCiteKeyButton, qtbot)
+    assert cGetTextAlpha(widget.rightPanel.editCiteKeyEntry) == 255
 
 
 # =============
