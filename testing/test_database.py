c8a281d1a5fbc59467e5c2380df575676213179b
Gillen Brown <gillenbrown@gmail.com>
2022-12-16 09:41:23 -0700

    fix to previous commit

--- testing/test_database.py
+++ testing/test_database.py
@@ -1415,7 +1415,7 @@ def test_import_with_multiline_authors_parses_correctly(db_empty, monkeypatch):
     db_empty.import_bibtex(file_loc)
     file_loc.unlink()  # delete before tests may fail
     assert parsed_values["author"] == (
-        "{Abadi}, M.~G. and {Navarro}, J.~F. and {Steinmetz}, M. and {Eke}, V.~R."
+        "{Abadi}, M. G. and {Navarro}, J. F. and {Steinmetz}, M. and {Eke}, V. R."
     )
     assert parsed_values["title"] == (
         "{Simulations of Galaxy Formation in a {$\\Lambda$} "
